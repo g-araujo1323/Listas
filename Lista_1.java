97efe1296ad484e16db618c956a861770c22b1d0
Gabriel Araujo <gaa1323@gmail.com>
2022-09-08 21:44:24 -0300

    Listas feitas em aula

---
+++ Lista_1.java
@@ -0,0 +1,169 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package lista_1;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Alunos
+ */
+public class Lista_1 {
+
+    
+    public static void main(String[] args) {
+        
+        // Gabriel de Almeida Araujo (RA: 822134612) e Henrique Mandri da Costa (RA: 822227761)
+        
+        //Letra A:
+        
+        /* double resultado;
+        
+        Scanner scan = new Scanner (System.in);
+    
+        System.out.println("Entre com o primeiro valor:");
+        double valor1 = scan.nextDouble();
+        
+        System.out.println("Entre com o segundo valor:");
+        double valor2 = scan.nextDouble();
+        
+        System.out.println("Entre com o terceiro valor:");
+        double valor3 = scan.nextDouble();
+        
+        resultado = valor1 * valor2 * valor3;
+        System.out.println("O resultado é: " + resultado);
+        */
+        
+        //Letra B:
+        
+        /*
+        
+        double media, nota1p, nota2p, nota3p;
+        int p1, p2;
+        p1 = 3;
+        p2 = 4;
+                
+        
+        Scanner scan = new Scanner (System.in);
+    
+        System.out.println("Entre com a primeira nota:");
+        double nota1 = scan.nextDouble();
+              
+        System.out.println("Entre com a segunda nota:");
+        double nota2 = scan.nextDouble();
+        
+        System.out.println("Entre com a terceira nota:");
+        double nota3 = scan.nextDouble();
+        
+        nota1p = nota1 * p1;
+        nota2p = nota2 * p1;
+        nota3p = nota3 * p2;
+                
+        media = (nota1p + nota2p + nota3p) /3;
+        System.out.printf("a media é: %.2f %n ", (media));
+        
+        */
+        
+        //letra C:
+        
+        /*
+        Scanner scan = new Scanner (System.in);
+        
+        System.out.println("Entre com o salário atual: ");
+        double salarioA = scan.nextDouble();
+        double salarioF;
+        
+        salarioF = salarioA * 1.25; 
+        
+        System.out.println("o novo salario é: " + salarioF);
+        */
+        
+        //Letra E:
+        
+        /*
+        
+        Scanner scan = new Scanner (System.in);
+    
+        System.out.println("Entre com o valor da base:");
+        double base = scan.nextDouble();
+        
+        System.out.println("Entre com o valor da altura:");
+        double altura = scan.nextDouble();
+        
+        
+        double resultado = base * altura; 
+        System.out.println("a área do retângulo é: " + resultado+ " ao quadrado");
+        
+        */
+        
+        //Letra F:
+        
+        /*
+        
+        Scanner scan = new Scanner (System.in);
+    
+        System.out.println("Entre com o valor da base:");
+        double base = scan.nextDouble();
+        
+        System.out.println("Entre com o valor da altura:");
+        double altura = scan.nextDouble();
+        
+        
+        double resultado = (base * altura) / 2; 
+        System.out.println("a área do triângulo é: " + resultado+ " ao quadrado");
+        
+        */
+        
+        //Letra J:
+        
+        /*
+        
+        
+        double media, nota1p, nota2p;
+        int p1, p2;
+        p1 = 2;
+        p2 = 3;
+                
+        
+        Scanner scan = new Scanner (System.in);
+    
+        System.out.println("Entre com a primeira nota:");
+        double nota1 = scan.nextDouble();
+              
+        System.out.println("Entre com a segunda nota:");
+        double nota2 = scan.nextDouble();
+               
+        nota1p = nota1 * p1;
+        nota2p = nota2 * p2;
+                        
+        media = (nota1p + nota2p) /2;
+        System.out.println("a media é: " + media);
+        
+        */
+        
+        //Letra L:
+        
+        /*
+        
+        int anoNasc, idaAtual, anoAtual;
+        
+        
+        Scanner scan = new Scanner (System.in);
+        
+        System.out.println("Entre com o seu ano de nascimento:");
+        anoNasc = scan.nextInt();
+              
+        System.out.println("Digite o Ano atual:");
+        anoAtual = scan.nextInt();
+        
+        idaAtual =  anoAtual - anoNasc; 
+        System.out.println("A sua idade atual é: " + idaAtual);
+        
+        */
+    }
+    
+    
+}
